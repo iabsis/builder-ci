c37db0bb7fd8a07b2fd376719e44d498348ec821
Olivier Bitsch <olivier.b@iabsis.com>
2024-11-04 11:26:16 +0100

    Bool on mandatory option

--- container/models.py
+++ container/models.py
@@ -16,8 +16,16 @@ class Container(models.Model):
     dockerfile = models.TextField(blank=True, validators=[validator.validate_dockerfile], help_text="Define the Dockerfile content, use {{var}} for automatic replacement for key defined in options.")
     target_tag = models.CharField(max_length=50, default='builder-{{image}}-{{tag}}')
     default_options = models.JSONField(default=dict, null=True, blank=True)
+    options_are_mandatory = models.BooleanField(default=False, help_text="Define if {{var}} defined in are mandatory in build request, otherwise fallback to default options that must be defined.")
 
     def clean(self):
+        super().clean()
+        if self.options_are_mandatory:
+            if self.default_options != None:
+                raise ValidationError(
+                    "Don't define default options if options are mandatory.", code="invalid")
+            else:
+                return
         if self.default_options == None:
             raise ValidationError(
                 'Default option must not be empty and must at least contain empty dictionnary {}.', code="invalid")
@@ -28,18 +36,28 @@ class Container(models.Model):
             raise ValidationError(
                 f'You defined variable, but is missing in default options: {e}', code="invalid")
 
-        super().clean()
-
     def __str__(self):
         return self.name
     
+    def merge_options(self, variables):
+        options = self.default_options
+        for key, value in variables.items():
+            options[key] = value
+        return options
+
     def render_dockerfile(self, **variables):
         template = Template(self.dockerfile, undefined=StrictUndefined)
-        return template.render(variables)
+        if self.options_are_mandatory:
+            return template.render(**variables)
+        else:
+            return template.render(**self.merge_options(variables))
     
     def get_target_tag(self, **variables):
         template = Template(self.target_tag, undefined=StrictUndefined)
-        return template.render(variables)
+        if self.options_are_mandatory:
+            return template.render(**variables)
+        else:
+            return template.render(**self.merge_options(variables))
 
 
 class Status(models.TextChoices):
