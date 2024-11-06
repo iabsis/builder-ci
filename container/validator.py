d66a602c81d5f336aec6fedf11c727e5ada6bae5
Olivier Bitsch <olivier.b@iabsis.com>
2024-11-06 08:56:20 +0100

    Add redirect for the home page

--- container/validator.py
+++ container/validator.py
@@ -1,9 +1,11 @@
 import re
 from django.core.exceptions import ValidationError
 from dockerfile_parse import DockerfileParser
+from io import BytesIO
 
 def validate_dockerfile(value):
-    dfp = DockerfileParser()
+    dockerfile = BytesIO()
+    dfp = DockerfileParser(fileobj=dockerfile)
     dfp.content = value
     if not dfp.baseimage:
         raise ValidationError(f"The base image is not defined")
