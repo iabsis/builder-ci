368f16eadec57f4f0407a69dd79648e4906bdcea
Olivier Bitsch <olivier.b@iabsis.com>
2024-11-27 08:28:26 +0100

    Save refactor but it's broken

---
+++ build/actions.py
@@ -0,0 +1,121 @@
+from . import models
+import os
+import logging
+from .notification import send_notification
+import container
+from podman import PodmanClient
+from podman.errors import ContainerError
+from django.conf import settings
+from git import Repo
+
+logger = logging.getLogger(__name__)
+
+def clone_repository(task, builddir):
+    logger.info(f'created temporary directory: {builddir}')
+    cloned_repo = Repo.clone_from(
+        task.build.request.url, os.path.join(builddir, "sources"), depth=1, branch=task.build.request.branch, single_branch=True)
+
+    task.build.meta['commit_id'] = cloned_repo.head.object.hexsha
+
+def fetch_version(task, builddir):
+    exception = None
+    try:
+        version_file = os.path.join(builddir, "sources", task.build.flow.version_file)
+        if models.BuildRequestMode.ON_VERSION in task.build.request.modes and not task.build.request.is_tag:
+            logger.info(f"Regex to use: {task.build.flow.version_regex}")
+            task.build.version = task.build.flow.get_version(version_file)
+
+        if models.BuildRequestMode.ON_TAG in task.build.request.modes and task.build.request.is_tag:
+            logger.info(f"Regex to use: {task.build.flow.version_regex}")
+            task.build.version = task.build.flow.replace_version(version_file, task.build.request.branch)
+    except Exception as e:
+        exception = e
+        
+    if not task.build.version and task.build.flow.version_mandatory:
+        message = f"Version is missing while flow requires a version, {exception}"
+        task.logs = message
+        task.save()
+        raise Exception(message)
+        
+    if not task.build.version and not task.build.flow.version_mandatory:
+        task.status = models.Status.ignored
+        message = f"Version is missing but flow DOESN'T require a version: {exception}"
+        task.logs = message
+        task.save()
+
+def duplicates_check(task, builddir):
+    if models.Build.objects.filter(
+        request__name=task.build.request.name,
+        flow=task.build.flow,
+        version=task.build.version,
+        status=models.Status.success,
+    ).exclude(
+        version=''
+    ).exclude(
+        version__isnull=True
+    ).exists():
+        task.build.status = models.Status.duplicate
+        send_notification(task.build)
+        raise Exception("Same success version found, stopping")
+
+def build_container_image(task, builddir):
+    try:
+        return container.tasks.build_image(
+            task.method.container.pk, task.options)
+    except Exception as e:
+        logger.debug(e)
+        raise Exception(e)
+
+def build_action(task, builtcontainer_name, builddir):
+# Create executable script and make it executable
+    script_file = os.path.join(builddir, "sources", 'run')
+    with open(script_file, '+w') as f:
+        f.write(task.script)
+    os.chmod(script_file, 0o755)
+
+
+    with PodmanClient(base_url=settings.PODMAN_URL) as client:
+        mounts = [
+            {
+                "target": "/build",
+                "read_only": False,
+                "source": builddir,
+                "type": "bind"
+            },
+            {
+                "target": "/run/podman/podman.sock",
+                "read_only": False,
+                "source": settings.PODMAN_PATH,
+                "type": "bind"
+            }
+        ]
+
+        try:
+            output = client.containers.run(
+                privileged=True,
+                image=builtcontainer_name,
+                remove=True,
+                environment=task.method.serialized_secrets,
+                stderr=True,
+                mounts=mounts,
+                network_mode="host",
+                entrypoint=['/build/sources/run'],
+                working_dir='/build/sources/',
+                user='0',
+            )
+            task.logs = output.decode()
+        
+        except ContainerError as e:
+            logger.error(e.stderr)
+            logs = "".join([line.decode() for line in e.stderr])
+            task.logs = f"exception occured executing task: {e}, {logs}"
+            task.status = models.Status.failed
+            task.build.save()
+            if task.method.stop_on_failure:
+                raise Exception("A mandatory task failed, stopping")
+        except Exception as e:
+            task.status = models.Status.failed
+            task.logs = f"exception occured executing task: {e}"
+            task.build.save()
+            if task.method.stop_on_failure:
+                raise Exception("A mandatory task failed, stopping")
--- build/tasks.py
+++ build/tasks.py
@@ -4,12 +4,9 @@ import os
 import celery
 from django.utils import timezone
 import container.models
-from . import models
+from . import models, actions
 from flow.models import Flow, Task
-from podman import PodmanClient
-from podman.errors import ContainerError
 from django_celery_results.models import TaskResult
-from git import Repo
 from django.conf import settings
 from .executor import BuildTaskExecutor
 from .notification import send_notification
@@ -52,58 +49,21 @@ def build_run(self, build_id):
 
     # Execute everything in a temporary folder
     with build.tmpfolder as tmpdirname:
-        logger.info(f'created temporary directory: {tmpdirname}')
-
+        
         with BuildTaskExecutor(build, "Cloning repository") as task:
-            cloned_repo = Repo.clone_from(
-                build.request.url, os.path.join(tmpdirname, "sources"), depth=1, branch=build.request.branch, single_branch=True)
-
-
-        build.meta['commit_id'] = cloned_repo.head.object.hexsha
+            actions.clone_repository(task, tmpdirname)
         
         # logger.info(cloned_repo.active_branch)
 
         ### VERSION FETCHING ##
-        try:
-            with BuildTaskExecutor(build, "Version fetching") as task:
-                version_file = os.path.join(tmpdirname, "sources", build.flow.version_file)
-                if models.BuildRequestMode.ON_VERSION in build.request.modes and not build.request.is_tag:
-                    logger.info(f"Regex to use: {build.flow.version_regex}")
-                    build.version = build.flow.get_version(version_file)
+        with BuildTaskExecutor(build, "Version fetching") as task:
+            actions.fetch_version(task, tmpdirname)
 
-                if models.BuildRequestMode.ON_TAG in build.request.modes and build.request.is_tag:
-                    logger.info(f"Regex to use: {build.flow.version_regex}")
-                    build.version = build.flow.replace_version(version_file, build.request.branch)
-                
-                if not build.version and build.flow.version_mandatory:
-                    raise Exception("Version is missing while flow requires a version")
-        except Exception as e:
-            if build.flow.version_mandatory:
-                message = f"Version is missing while flow requires a version: {e}"
-                raise Exception(message)
-            else:
-                task.status = models.Status.ignored
-                message = f"Version is missing but flow DOESN'T require a version: {e}"
-            task.logs = message
-            task.save()
         build.save()
 
         ### DUPLICATES CHECK ##
         with BuildTaskExecutor(build, "Duplicates check") as task:
-            if models.Build.objects.filter(
-                request__name=build.request.name,
-                flow=build.flow,
-                version=build.version,
-                status=models.Status.success,
-            ).exclude(
-                version=''
-            ).exclude(
-                version__isnull=True
-            ).exists():
-                build.status = models.Status.duplicate
-                send_notification(build)
-                raise Exception("Same success version found, stopping")
-
+            actions.duplicates_check(task, tmpdirname)
 
         ### TASKS DEFINITION ##
         for task in Task.objects.filter(flow=build.flow).order_by('priority'):
@@ -127,69 +87,12 @@ def build_run(self, build_id):
 
             send_notification(build)
 
-            with BuildTaskExecutor(buildtask=build_task.image_task) as _:
-                try:
-                    builtcontainer_name = container.tasks.build_image(
-                        build_task.method.container.pk, build_task.options)
-                except Exception as e:
-                    logger.debug(e)
-                    raise Exception(e)
+            with BuildTaskExecutor(buildtask=build_task.image_task) as task:
+                builtcontainer_name = actions.build_container_image(build_task.image_task, tmpdirname)
     
             with BuildTaskExecutor(buildtask=build_task) as task:
-
-                # Create executable script and make it executable
-                script_file = os.path.join(tmpdirname, "sources", 'run')
-                with open(script_file, '+w') as f:
-                    f.write(build_task.script)
-                os.chmod(script_file, 0o755)
-
-
-                with PodmanClient(base_url=settings.PODMAN_URL) as client:
-                    mounts = [
-                        {
-                            "target": "/build",
-                            "read_only": False,
-                            "source": tmpdirname,
-                            "type": "bind"
-                        },
-                        {
-                            "target": "/run/podman/podman.sock",
-                            "read_only": False,
-                            "source": settings.PODMAN_PATH,
-                            "type": "bind"
-                        }
-                    ]
-
-                    try:
-
-                        output = client.containers.run(
-                            privileged=True,
-                            image=builtcontainer_name,
-                            remove=True,
-                            environment=task.method.serialized_secrets,
-                            stderr=True,
-                            mounts=mounts,
-                            network_mode="host",
-                            entrypoint=['/build/sources/run'],
-                            working_dir='/build/sources/',
-                            user='0',
-                        )
-                        task.logs = output.decode()
-                    
-                    except ContainerError as e:
-                        logger.error(e.stderr)
-                        logs = "".join([line.decode() for line in e.stderr])
-                        build_task.logs = f"exception occured executing task: {e}, {logs}"
-                        build_task.status = models.Status.failed
-                        build.save()
-                        if build_task.method.stop_on_failure:
-                            raise Exception("A mandatory task failed, stopping")
-                    except Exception as e:
-                        build_task.status = models.Status.failed
-                        build_task.logs = f"exception occured executing task: {e}"
-                        build.save()
-                        if build_task.method.stop_on_failure:
-                            raise Exception("A mandatory task failed, stopping")
+                actions.build_action(task, builtcontainer_name, tmpdirname)
+                
 
     # Set to None will automatically detect the status
     build.status = models.Status.success
